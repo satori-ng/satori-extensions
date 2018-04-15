2a5fccd4f9dd181e5d6795a1765b44fcd7fe8b26
operatorequals <john.torakis@gmail.com>
2018-04-15 19:31:56 +0300

    Added hash and magicbyte extensions

---
+++ hash/sha160.py
@@ -0,0 +1,20 @@
+import binascii
+import hashlib
+
+from satoricore.hooker import hook
+
+__name__ = 'sha160'
+
+@hook("with_open")
+def hash_file(satori_image, file_path, file_type, fd):
+    fd.seek(0)
+    hash_obj = hashlib.new(__name__)
+
+    n_chunk = 1024**2
+    bytechunk = fd.read(n_chunk)
+    while bytechunk:
+        hash_obj.update(bytechunk)
+        bytechunk = fd.read(n_chunk)
+    
+    hex_digest = hash_obj.hexdigest()
+    satori_image.set_attribute(file_path, hex_digest, __name__, force_create=True)
